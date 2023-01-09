530aebb0e1ea5c26829b0a4a61d514a9bf7e41ea
thinn <thinnphm826@gmail.com>
2023-01-09 15:45:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/everest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.everest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("firstName", "Andy");
+        user.put("lastName", "Pham");
+
+        firestore.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
