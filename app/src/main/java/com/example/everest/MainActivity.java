2b16c651994d3b1df568bcc1d292049e99d9f018
thinn <thinnphm826@gmail.com>
2023-01-11 12:20:06 +0700

    Update new view, implement picasso to display pictures

--- app/src/main/java/com/example/everest/MainActivity.java
+++ app/src/main/java/com/example/everest/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.everest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -14,20 +17,30 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseFirestore firestore;
+    FirebaseFirestore fireStore;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firestore = FirebaseFirestore.getInstance();
+        fireStore = FirebaseFirestore.getInstance();
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AddBook.class);
+                startActivity(i);
+            }
+        });
 
         Map<String, Object> user = new HashMap<>();
-        user.put("firstName", "Andy");
-        user.put("lastName", "Pham");
+//        user.put("firstName", "Andy");
+//        user.put("lastName", "Pham");
 
-        firestore.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        fireStore.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
                 Toast.makeText(getApplicationContext(),"success", Toast.LENGTH_SHORT).show();
