c91a9e3daa898c872d3e30f648d3968bad09859a
thinn <thinnphm826@gmail.com>
2023-01-11 16:03:55 +0700

    continue to add book

--- app/src/main/java/com/example/everest/AddBook.java
+++ app/src/main/java/com/example/everest/AddBook.java
@@ -3,12 +3,30 @@ package com.example.everest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddBook extends AppCompatActivity {
+
+    Button addButton;
+    EditText nameText, authorText, priceText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
+
+        addButton = (Button) findViewById(R.id.button);
+        nameText = (EditText) findViewById(R.id.bookName);
+        authorText = (EditText) findViewById(R.id.authorName);
+        priceText = (EditText) findViewById(R.id.bookPrice);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/everest/User.java
@@ -0,0 +1,13 @@
+package com.example.everest;
+
+import java.util.List;
+
+public class User {
+    String name;
+    String password;
+    String age;
+    String phoneNo;
+    String address;
+
+    List<Book> buyBookList;
+}
