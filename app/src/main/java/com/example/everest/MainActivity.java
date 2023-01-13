e83b6f0af2d4ff550c2f8033ea5d552cdcdd58af
Kiet Anh Park <s3681475@rmit.edu.vn>
2023-01-13 12:13:33 +0700

    đmm ân

--- app/src/main/java/com/example/everest/MainActivity.java
+++ app/src/main/java/com/example/everest/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.everest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -13,14 +16,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseAuth mAuth;
     FirebaseFirestore fireStore;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         fireStore = FirebaseFirestore.getInstance();
 
         Map<String, Object> user = new HashMap<>();
@@ -30,8 +33,17 @@ public class MainActivity extends AppCompatActivity {
         fireStore.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
-                Toast.makeText(getApplicationContext(),"success", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(MainActivity.this, SignUp.class));
+        }
     }
 }
