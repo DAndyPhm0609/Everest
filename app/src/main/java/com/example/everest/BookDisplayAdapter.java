ef736668d9f73513adbda9c8dade9203add4468c
Kiet Anh Park <s3681475@rmit.edu.vn>
2023-01-13 16:04:28 +0700

    add all file

---
+++ app/src/main/java/com/example/everest/BookDisplayAdapter.java
@@ -0,0 +1,57 @@
+package com.example.everest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class BookDisplayAdapter extends RecyclerView.Adapter<BookDisplayAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<BookData> list;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView bookCover;
+        private TextView name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bookCover= itemView.findViewById(R.id.bookCover);
+            name = itemView.findViewById(R.id.bookName);
+        }
+    }
+
+    public BookDisplayAdapter(Context context, ArrayList<BookData> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View heroView = inflater.inflate(R.layout.item_column, parent, false);
+        ViewHolder viewHolder = new ViewHolder(heroView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        BookData book = list.get(position);
+        Glide.with(context).load(book.getBookCover()).into(holder.bookCover);
+        holder.name.setText(book.getBookName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
---
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.everest.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<BookData> list;
+    private RecyclerView recyclerBook;
+    private BookCoverAdapter bookCoverAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerBook = findViewById(R.id.recyclerView);
+        list = new ArrayList<>();
+        createBookList();
+        bookCoverAdapter = new BookCoverAdapter(this, list);
+        recyclerBook.setAdapter(bookCoverAdapter);
+        recyclerBook.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void createBookList() {
+        list.add(new BookData("Harry Porter", R.drawable.hp_cover));
+        list.add(new BookData("Harry Porter", R.drawable.hp_cover));
+    }
+}
