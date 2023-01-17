907dbaf7b4c3dc9e98b1216dc9b5032557a3e557
alice <uyennmd>
2023-01-17 13:29:10 +0700

    select all button

--- app/src/main/java/com/example/everest/CartListAdapter.java
+++ app/src/main/java/com/example/everest/CartListAdapter.java
@@ -1,25 +1,26 @@
 package com.example.everest;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.Collections;
 import java.util.List;
 
-public class CartListAdapter extends RecyclerView.Adapter<cartViewHolder> {
-
-    public static boolean isSelectedAll = false;
-    List<Book> list
-            = Collections.emptyList();
+public class CartListAdapter extends RecyclerView.Adapter<CartListAdapter.cartViewHolder> {
+    List<Book> list;
     static SparseBooleanArray itemStateArray= new SparseBooleanArray();
     Context context;
     ClickListener listener;
-
     public CartListAdapter(List<Book> list,
                                  Context context,ClickListener listener)
     {
@@ -29,6 +30,7 @@ public class CartListAdapter extends RecyclerView.Adapter<cartViewHolder> {
     }
 
 
+    @NonNull
     @Override
     public cartViewHolder
     onCreateViewHolder(ViewGroup parent,
@@ -43,12 +45,10 @@ public class CartListAdapter extends RecyclerView.Adapter<cartViewHolder> {
                 = inflater
                 .inflate(R.layout.cart_card,
                         parent, false);
-        cartViewHolder viewHolder
-                = new cartViewHolder(photoView);
-        return viewHolder;
+        return new cartViewHolder(photoView);
     }
 
-    @Override
+    @SuppressLint("SetTextI18n")
     public void
     onBindViewHolder(final cartViewHolder viewHolder,
                      final int position)
@@ -59,10 +59,10 @@ public class CartListAdapter extends RecyclerView.Adapter<cartViewHolder> {
         viewHolder.cartAuthor
                 .setText(list.get(position).author);
         viewHolder.cartPrice
-                .setText("$" + String.valueOf(list.get(position).price));
-        if (!isSelectedAll) viewHolder.buyCheckBox.setChecked(false);
-        else viewHolder.buyCheckBox.setChecked(true);
-        viewHolder.bind(position);
+                .setText("$" + list.get(position).price);
+        viewHolder.buyCheckBox.setChecked(itemStateArray.get(position));
+        viewHolder.buyCheckBox.setOnClickListener(v -> checkCheckBox(index, !itemStateArray.get(index)));
+        viewHolder.cartCard.setOnClickListener(view -> Toast.makeText(view.getContext(),String.valueOf(index),Toast.LENGTH_SHORT).show());
     }
     @Override
     public int getItemCount()
@@ -73,18 +73,63 @@ public class CartListAdapter extends RecyclerView.Adapter<cartViewHolder> {
         return list.size();
     }
 
-    public static void selectAll(){
-        isSelectedAll=true;
-    }
-
     @Override
     public void onAttachedToRecyclerView(
-            RecyclerView recyclerView)
+            @NonNull RecyclerView recyclerView)
     {
         super.onAttachedToRecyclerView(recyclerView);
     }
     public interface ClickListener{
-        void click(int index);
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void removeSelection() {
+        itemStateArray = new SparseBooleanArray();
+        notifyDataSetChanged();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void checkCheckBox(int position, boolean value) {
+        if (value)
+            itemStateArray.put(position, true);
+        else
+            itemStateArray.delete(position);
+        notifyDataSetChanged();
+    }
+
+    static class cartViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout cartCard;
+        CheckBox buyCheckBox;
+        ImageView cartPhoto;
+        TextView cartName;
+        TextView cartAuthor;
+        TextView cartPrice;
+        @SuppressLint("StaticFieldLeak")
+        static View view;
+        cartViewHolder(View itemView)
+        {
+            super(itemView);
+            buyCheckBox
+                    = itemView
+                    .findViewById(R.id.buyCheckBox);
+            cartPhoto
+                    = itemView
+                    .findViewById(R.id.cartPhoto);
+            cartName
+                    = itemView
+                    .findViewById(R.id.cartName);
+            cartAuthor
+                    = itemView
+                    .findViewById(R.id.cartAuthor);
+            cartPrice
+                    = itemView
+                    .findViewById(R.id.cartPrice);
+            view  = itemView;
+            cartCard
+                    = itemView
+                    .findViewById(R.id.cartCard);
+        }
     }
 }
+
 
--- app/src/main/java/com/example/everest/ShowCart.java
+++ app/src/main/java/com/example/everest/ShowCart.java
@@ -4,13 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,20 +25,14 @@ public class ShowCart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_cart);
-        recyclerView = findViewById(R.id.cartList);
         selectAll = findViewById(R.id.selectAll);
-        total = findViewById(R.id.total);
-        amount = findViewById(R.id.amount);
-        checkout = findViewById(R.id.checkout);
-        listener = new CartListAdapter.ClickListener() {
-            @Override
-            public void click(int index){
-//                Intent edit = new Intent(MainActivity.this, BookDetail.class);
-//                edit.putExtra("index",index);
-//                startActivityForResult(edit,300);
-                Toast.makeText(ShowCart.this,Integer.toString(index), Toast.LENGTH_SHORT).show();
-            }
-        };
+        populateRecyclerView();
+        onClickEvent();
+    }
+    private void populateRecyclerView() {
+        recyclerView = findViewById(R.id.cartList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ShowCart.this));
         cartList.add(new Book("Harry Potter", "J. K. Rowling", 30, "5.0", "fantasy", "US", 1));
         cartList.add(new Book("Harry Potter", "J. K. Rowling", 30, "5.0", "fantasy", "US", 1));
         cartList.add(new Book("Harry Potter", "J. K. Rowling", 30, "5.0", "fantasy", "US", 1));
@@ -52,15 +43,27 @@ public class ShowCart extends AppCompatActivity {
         cartList.add(new Book("Harry Potter", "J. K. Rowling", 30, "5.0", "fantasy", "US", 1));
 
         adapter = new CartListAdapter(cartList, getApplication(),listener);
-        selectAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CartListAdapter.selectAll();
-                adapter.notifyDataSetChanged();
-            }
-        } );
         recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(ShowCart.this));
-        adapter.notifyDataSetChanged();
+        listener = new CartListAdapter.ClickListener() {
+
+        };
+    }
+
+    private void onClickEvent() {
+        selectAll.setOnClickListener(view -> {
+            //Check the current text of Select Button
+            if (selectAll.getText().toString().equals(getResources().getString(R.string.select_all))) {
+                //If Text is Select All then loop to all array List items and check all of them
+                for (int i = 0; i < cartList.size(); i++)
+                    adapter.checkCheckBox(i, true);
+                //After checking all items change button text
+                selectAll.setText(getResources().getString(R.string.deselect_all));
+            } else {
+                //If button text is Deselect All remove check from all items
+                adapter.removeSelection();
+                //After checking all items change button text
+                selectAll.setText(getResources().getString(R.string.select_all));
+            }
+        });
     }
 }
