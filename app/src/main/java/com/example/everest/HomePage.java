3da571d6aef0bc9e4518ac3a043673ad6718b37d
thuy <s3877746@rmit.edu.vn>
2023-01-19 17:09:38 +0700

    edit book detail activity, add list view in homepage

--- app/src/main/java/com/example/everest/HomePage.java
+++ app/src/main/java/com/example/everest/HomePage.java
@@ -12,31 +12,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.everest.databinding.ActivityHomePageBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class HomePage extends AppCompatActivity {
-    private ArrayList<BookData> list = new ArrayList<>();
+    private ArrayList<BookData> list;
+    private ArrayList<Book> newList;
     private RecyclerView recyclerBook;
     private static final String TAG = "HomePage";
-    ActivityHomePageBinding binding;
+    private TextView welcomeView;
 
     Button addButton;
     FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
@@ -47,21 +41,19 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_home_page);
         recyclerBook = findViewById(R.id.recyclerView);
 
-        String[] name;
-        String[] author;
-        String[] price;
-        ImageButton[] wishList;
-        Image[] star;
-        Image[] bookCover;
+        welcomeView = (TextView) findViewById(R.id.welcomeText);
+        String userName;
+
+        Intent i = getIntent();
+        userName = (String) i.getStringExtra("name");
+        System.out.println("From homepage" + userName);
+
+        welcomeView.setText(String.format("Hello: %s", userName));
 
 //        addButton = (Button) findViewById(R.id.addButton);
-//
-//        list = new ArrayList<>();
-//        createBookList();
 //
 //        addButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -70,6 +62,9 @@ public class HomePage extends AppCompatActivity {
 //                startActivity(i);
 //            }
 //        });
+
+        list = new ArrayList<>();
+        createBookList();
 
         bookAdapter = new BookDisplayAdapter(this, list);
         recyclerBook.setAdapter(bookAdapter);
